6f2f3bf4549e3439ba82edd55484ba9428c92473
niilz <niilz@outlook.de>
2023-11-19 13:22:24 +0100

    Test derive to_proto

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 extern crate proud;
-
 use proud::{generate_structs, ProtoBuf};
 
-fn main() {
-    #[derive(ProtoBuf)]
-    struct AnyStruct;
+#[derive(ProtoBuf)]
+struct AnyStruct;
 
+#[derive(ProtoBuf)]
+struct PersonRs {
+    name: String,
+}
+
+fn main() {
+    // Generate structs from proto-file
     generate_structs!("person.proto");
 
-    /*
-    let person = Person {
-        name: "Foo".to_string();
+    // Generate proto data from rust-struct
+    let person = PersonRs {
+        name: "person".to_string(),
     };
-    */
+
+    let proto_buf = person.to_proto();
+    println!("{proto_buf}");
 }
